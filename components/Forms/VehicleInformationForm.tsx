eaf217e24d6e09a26d4ca61ff29bbce42f607c6f
Oshoke Oyati <oyatioshoke@yahoo.com>
2023-05-02 15:42:24 +0100

    documents

--- components/Forms/VehicleInformationForm.tsx
+++ components/Forms/VehicleInformationForm.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Typography from '../Typography';
 import classNames from 'classnames';
 import Button from '../Button';
 import { useRouter } from 'next/router';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
 
 const labelClasses = classNames(
   'block text-[14px] leading-[14px] font-gordita-medium text-brandGray-300'
@@ -10,6 +11,14 @@ const labelClasses = classNames(
 
 const inputClasses = classNames(
   `px-2  h-[40px] py-2 border border-[#d4d6d8] rounded-lg mt-3  w-full font-gordita-regular`
+);
+
+const dragAndDropClasses = classNames(
+  `font-gordita-medium my-2 text-[10px] text-[#41454C] `
+);
+
+const typeDocClasses = classNames(
+  `text-[#797980] font-gordita-regular text-[12px] leading-[17px] `
 );
 
 const vehicleFeatures = [
@@ -31,6 +40,13 @@ const VehicleInformationForm = () => {
     e.preventDefault();
     router.push('/put-up-your-vehicle/documents');
   };
+
+  const [dragEnter, setDragEnter] = useState(false);
+
+  const [exteriorFront, setExteriorFront] = useState<{ name: string } | null>();
+  const [exteriorBack, setExteriorBack] = useState<{ name: string } | null>();
+
+  const wrapperRef = useRef(null);
 
   return (
     <main className='w-[90%] lg:mx-auto lg:max-w-[500px]'>
@@ -128,7 +144,68 @@ const VehicleInformationForm = () => {
                 );
               })}
             </div>
-            <div>DRAG AND DROP COMPONENENT</div>
+
+            <div className='grid grid-cols-2 gap-[32px]'>
+              <div
+                ref={wrapperRef}
+                className='drop-file-input bg-[#f9fafb]
+                '
+              >
+                <span className=''>
+                  <AiOutlineCloudUpload className='w-7 h-7' />
+                </span>
+
+                <div className={dragAndDropClasses}>
+                  Drag and drop or Choose file to upload
+                </div>
+                <span className={typeDocClasses}>
+                  {exteriorFront?.name || 'Exterior front(with headlights on)'}
+                </span>
+                <input
+                  multiple={false}
+                  type='file'
+                  value=''
+                  required
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    if (!e.target.files) {
+                      return;
+                    }
+                    setExteriorFront(e.target.files[0]);
+                  }}
+                />
+              </div>
+
+              <div
+                ref={wrapperRef}
+                className='drop-file-input bg-[#f9fafb]
+                '
+              >
+                <span className=''>
+                  <AiOutlineCloudUpload className='w-7 h-7' />
+                </span>
+
+                <div className={dragAndDropClasses}>
+                  Drag and drop or Choose file to upload
+                </div>
+                <span className={typeDocClasses}>
+                  {exteriorBack?.name ||
+                    'Exterior back (with reverse light on)'}
+                </span>
+                <input
+                  multiple={false}
+                  type='file'
+                  value=''
+                  required
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    if (!e.target.files) {
+                      return;
+                    }
+                    setExteriorBack(e.target.files[0]);
+                  }}
+                />
+              </div>
+            </div>
+
             <Button
               bg='bg-brandGreen-300'
               hover='hover:bg-brandGray-200'
