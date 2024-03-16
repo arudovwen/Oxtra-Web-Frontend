07be7d4b487f77f53bf4657cff10274a0ab3cd35
Billioma <ibeomari@gmail.com>
2024-03-16 10:56:46 +0100

    updates

--- layout/PageLayout.tsx
+++ layout/PageLayout.tsx
@@ -1,27 +1,26 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, useMediaQuery } from "@chakra-ui/react";
 import Header from "./AuthLayout/Header";
 import SideBar from "./AuthLayout/SideBar";
 
 const AuthLayout = ({ children }: any) => {
+  const [isMobile] = useMediaQuery("(max-width: 991px)");
   return (
-    <Flex minH="100vh" pos="relative" w="full" flexDir="column">
+    <Flex minH="100vh" pb="50px" pos="relative" w="full" flexDir="column">
       <Box pos="sticky" top="0" zIndex={10} bg="#fff">
         <Header />
       </Box>
 
       <Flex w="full" mt="40px" justifyContent="center" align="center">
         <Flex align="flex-start" pos="relative" w="1192px" gap="36px">
-          <Box w="30%" pos="sticky" top="135px">
+          <Box
+            display={isMobile ? "none" : "block"}
+            w="30%"
+            pos="sticky"
+            top="135px"
+          >
             <SideBar />
           </Box>
-          <Box
-            flexDir="column"
-            border="1px solid #E4E4E4"
-            borderRadius="16px"
-            h="42rem"
-            w="70%"
-            p="24px"
-          >
+          <Box w={isMobile ? "100%" : "70%"} px="20px">
             {children}
           </Box>
         </Flex>
