f6645678d3bdf9d31bd5f26276ec03310623218a
Oshoke Oyati <oyatioshoke@yahoo.com>
2024-04-03 09:18:00 +0100

    fix deployment error

--- pages/rent-a-car/[car_name]/booking.tsx
+++ pages/rent-a-car/[car_name]/booking.tsx
@@ -1,16 +1,16 @@
-import Container from "@/layout/NonAuthLayout/Container";
-import Navigation from "@/layout/NonAuthLayout/Navigation";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { RiArrowRightDoubleLine } from "react-icons/ri";
-import classNames from "classnames";
-import CarFeatures from "@/components/data/rent-a-car/booking/CarFeatures";
-import CarBooking from "@/components/data/rent-a-car/booking/CarBooking";
-import TripDetails from "@/components/data/rent-a-car/booking/TripDetails";
-import Breakdown from "@/components/data/rent-a-car/booking/Breakdown";
+import Container from '@/layout/NonAuthLayout/Container';
+import Navigation from '@/layout/NonAuthLayout/Navigation';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { RiArrowRightDownLine } from 'react-icons/ri';
+import classNames from 'classnames';
+import CarFeatures from '@/components/data/rent-a-car/booking/CarFeatures';
+import CarBooking from '@/components/data/rent-a-car/booking/CarBooking';
+import TripDetails from '@/components/data/rent-a-car/booking/TripDetails';
+import Breakdown from '@/components/data/rent-a-car/booking/Breakdown';
 
 const Bookings = () => {
-  const activePage = "Rent a car";
+  const activePage = 'Rent a car';
 
   const router = useRouter();
   const { car_name } = router.query;
@@ -19,61 +19,54 @@ const Bookings = () => {
   const [driverType, setDriverType] = useState("I'll drive by myself");
   const [availability, setAvailability] = useState(false);
 
-  const dataClasses = classNames("text-[#242424] font-gordita-bold");
+  const dataClasses = classNames('text-[#242424] font-gordita-bold');
 
-  const parClasses = classNames("flex items-center justify-between");
+  const parClasses = classNames('flex items-center justify-between');
 
   return (
-    <div className="font-gordita mb-[108px]">
+    <div className='font-gordita mb-[108px]'>
       <Container>
         <Navigation
-          color="text-brandGray-300"
-          hover="hover:text-brandGreen-300"
-          buttonBg="bg-brandGreen-300"
-          buttonText="text-white"
-          buttonHover="hover:bg-white"
+          color='text-brandGray-300'
+          hover='hover:text-brandGreen-300'
+          buttonBg='bg-brandGreen-300'
+          buttonText='text-white'
+          buttonHover='hover:bg-white'
           activePage={activePage}
-          navBackground="white"
-          menuColor="text-brandGreen-300"
+          navBackground='white'
+          menuColor='text-brandGreen-300'
         />
 
-        <div className="mt-[40px] md:mt-[72px]">
-          <div className="flex items-center gap-[16px] w-fit font-gordita-medium text-[12px] md:text-[16px]">
-            <div className={`text-[#444648]`} onClick={() => router.push("/")}>
+        <div className='mt-[40px] md:mt-[72px]'>
+          <div className='flex items-center gap-[16px] w-fit font-gordita-medium text-[12px] md:text-[16px]'>
+            <div className={`text-[#444648]`} onClick={() => router.push('/')}>
               Home
             </div>
-            <div className="mb-[3px]">
-              <RiArrowRightDoubleLine />
+            <div className='mb-[3px]'>
+              <RiArrowRightDownLine />
             </div>
-            <div
-              onClick={() => router.push("/rent-a-car")}
-              className="text-[#444648]"
-            >
+            <div onClick={() => router.push('/rent-a-car')} className='text-[#444648]'>
               Find Vehicle
             </div>
-            <div className="mb-[3px]">
-              <RiArrowRightDoubleLine />
+            <div className='mb-[3px]'>
+              <RiArrowRightDownLine />
             </div>
             <div
               onClick={() => {
                 setStep(2);
                 setAvailability(false);
               }}
-              className={`${
-                step === 2 ? "text-[#D4D4D4]" : "text-[#444648] cursor-pointer"
-              }`}
+              className={`${step === 2 ? 'text-[#D4D4D4]' : 'text-[#444648] cursor-pointer'}`}
             >
               {car_name}
             </div>
-            <div className={`${step === 3 ? "flex" : "hidden"} mb-[3px]`}>
-              <RiArrowRightDoubleLine />
+            <div className={`${step === 3 ? 'flex' : 'hidden'} mb-[3px]`}>
+              <RiArrowRightDownLine />
             </div>
             <div
               onClick={() => setStep(3)}
               className={`${
-                step === 3
-                  ? "text-[#D4D4D4] flex"
-                  : "text-[#444648] hidden cursor-pointer"
+                step === 3 ? 'text-[#D4D4D4] flex' : 'text-[#444648] hidden cursor-pointer'
               }`}
             >
               Booking Details
@@ -83,7 +76,7 @@ const Bookings = () => {
 
         <div
           className={`${
-            step === 3 ? "hidden" : "flex"
+            step === 3 ? 'hidden' : 'flex'
           } mt-[24px] flex items-start md:flex-row flex-col gap-[40px]`}
         >
           <CarFeatures />
@@ -98,7 +91,7 @@ const Bookings = () => {
 
         <div
           className={`${
-            step === 3 ? "flex" : "hidden"
+            step === 3 ? 'flex' : 'hidden'
           } flex-col md:flex-row items-start gap-[40px] mt-[32px]`}
         >
           <TripDetails
