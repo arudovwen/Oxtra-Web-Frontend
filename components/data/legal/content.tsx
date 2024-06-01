63b2c9df36768441d9215a1ee65a3b7431653d4f
Oshoke Oyati <oyatioshoke@yahoo.com>
2024-06-01 19:24:53 +0100

    add animation to home page

--- components/data/legal/content.tsx
+++ components/data/legal/content.tsx
@@ -6,7 +6,7 @@ const Content = ({ selectedTopic }: { selectedTopic: string }) => {
     <div className='border prose  w-full font-gordita-regular rounded-[16px] border-solid  p-[22px]'>
       <h2 className='text-sm text-brandGreen-400 font-semibold mb-[24px]'>{selectedTopic}</h2>
 
-      <p>{contentMap[selectedTopic].content}</p>
+      <div>{contentMap[selectedTopic].content}</div>
     </div>
   );
 };
