23405d80f2d2a96dfa61342b7f8871ffb5d8ec91
Success Ahon <successahon@gmail.com>
2024-06-02 20:17:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,9 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = withMDX(nextConfig);
